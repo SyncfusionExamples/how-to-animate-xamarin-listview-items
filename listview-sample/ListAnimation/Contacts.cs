5639e9eb8e2cee532ddd40e23cc928b258883a06
GnanaPriya N <gnanapriya.namasivayam@syncfusion.om>
2019-11-07 12:33:55 +0530

    Updated sample by removing visibility codes.

---
+++ listview-sample/ListAnimation/Contacts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ListAnimation
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string contactName;
+        private string contactNo;
+        private string contactType;
+
+        #endregion
+
+        #region Constructor
+
+        public Contacts()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string ContactName
+        {
+            get { return this.contactName; }
+            set
+            {
+                this.contactName = value;
+                RaisePropertyChanged("ContactName");
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNo; }
+            set
+            {
+                this.contactNo = value;
+                RaisePropertyChanged("ContactNumber");
+            }
+        }
+
+        public string ContactType
+        {
+            get { return contactType; }
+            set
+            {
+                this.contactType = value;
+                RaisePropertyChanged("ContactType");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
